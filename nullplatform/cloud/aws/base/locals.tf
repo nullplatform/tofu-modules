35cc26569a94c8cae1a5ab5f0d3ea82ef4cc4a92
David Fernandez <rodavidfernandez@gmail.com>
2025-10-10 12:11:53 -0300

    feat: remove custom base module

--- nullplatform/cloud/aws/base/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  nullplatform_base_values = templatefile("${path.module}/templates/nullplatform-base-values.tmpl.yaml", {
-    api_key = nullplatform_api_key.nullplatform-base-api-key.api_key
-  })
-}
--- nullplatform/cloud/gcp/base/locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  nrn_without_namespace = join(":", slice(split(":", var.nrn), 0, 2))
-  nullplatform_base_values = templatefile("${path.module}/templates/nullplatform-base-values.tmpl.yaml", {
-    api_key = nullplatform_api_key.nullplatform-base-api-key.api_key
-
-  })
-}
