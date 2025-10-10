c691827d6eaabc3602324a1d39bef8c780083bf9
sebas_correa <sebastian.correa@nullplatform.io>
2025-10-10 11:44:36 -0300

    feat(prometheus): delete local providers

--- nullplatform/prometheus/providers.tf
+++ nullplatform/prometheus/providers.tf
@@ -1,24 +1,8 @@
 terraform {
   required_providers {
-    nullplatform = {
-      source  = "nullplatform/nullplatform"
-      version = "~> 0.0.63"
-    }
-
     helm = {
       source  = "hashicorp/helm"
       version = "~> 3.0"
     }
   }
-}
-provider "helm" {
-  kubernetes = {
-    config_path    = var.kubeconfig_path
-    config_context = var.kube_context
-  }
-}
-
-provider "nullplatform" {
-
-  api_key = var.np_api_key
 }
