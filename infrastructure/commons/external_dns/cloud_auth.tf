30f3a7f2c81e97e4f66c7883446e408b2b0867e5
sebas_correa <sebastian.correa@nullplatform.io>
2025-11-06 14:05:35 -0300

    feat(commons): rquired varaibles

--- infrastructure/commons/external_dns/cloud_auth.tf
+++ infrastructure/commons/external_dns/cloud_auth.tf
@@ -1,6 +1,6 @@
 # GSA
 resource "google_service_account" "external_dns" {
-  for_each     = local.google_toggle
+  count        = var.dns_provider_name == "google" ? 1 : 0
   account_id   = var.gsa_name
   display_name = "ExternalDNS GSA"
   project      = var.project_id
@@ -8,17 +8,17 @@ resource "google_service_account" "external_dns" {
 
 # Permission Cloud DNS
 resource "google_project_iam_member" "external_dns_dns_admin" {
-  for_each   = local.google_toggle
+  count      = var.dns_provider_name == "google" ? 1 : 0
   project    = var.project_id
   role       = "roles/dns.admin"
-  member     = "serviceAccount:${google_service_account.external_dns["on"].email}"
+  member     = "serviceAccount:${google_service_account.external_dns[0].email}"
   depends_on = [google_service_account.external_dns]
 }
 
 # Workload Identity binding KSA -> GSA
 resource "google_service_account_iam_member" "wi_binding" {
-  for_each           = local.google_toggle
-  service_account_id = google_service_account.external_dns["on"].name
+  count              = var.dns_provider_name == "google" ? 1 : 0
+  service_account_id = google_service_account.external_dns[0].name
   role               = "roles/iam.workloadIdentityUser"
   member             = "serviceAccount:${var.project_id}.svc.id.goog[${var.external_dns_namespace}/${var.ksa_name}]"
   depends_on         = [google_project_iam_member.external_dns_dns_admin]
