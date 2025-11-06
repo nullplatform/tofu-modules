6360c94ceee3fcefec831f9cfeb12402e8857f2f
Gonza Rojas <gonzarojas@MacBook-Pro-de-Gonza.local>
2025-11-06 17:32:08 -0300

    fix: secret

--- infrastructure/commons/external_dns/main.tf
+++ infrastructure/commons/external_dns/main.tf
@@ -7,16 +7,6 @@ resource "kubernetes_namespace" "external_dns" {
   }
 }
 
-resource "kubernetes_secret_v1" "external_dns_cloudflare" {
-  metadata {
-    name      = "cloudflare-api-token"
-    namespace = kubernetes_namespace_v1.external_dns.metadata[0].name
-  }
-  data = {
-    api-token = var.cloudflare_api_token
-  }
-  type = "Opaque"
-}
 
 resource "helm_release" "external_dns" {
   name             = "external-dns"
