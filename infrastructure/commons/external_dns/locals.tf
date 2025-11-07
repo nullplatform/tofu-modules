6533243242978c9a1c7df784f477803d666a8b21
sebas_correa <sebastian.correa@nullplatform.io>
2025-11-07 11:32:48 -0300

    feat(external-dns): delete google provider

--- infrastructure/commons/external_dns/locals.tf
+++ infrastructure/commons/external_dns/locals.tf
@@ -1,7 +1,7 @@
 locals {
   create_cf_secret = lower(var.dns_provider_name) == "cloudflare"
-  use_google_dns   = lower(var.dns_provider_name) == "google"
-  google_toggle    = local.use_google_dns ? toset(["on"]) : toset([])
+  # use_google_dns   = lower(var.dns_provider_name) == "google"
+  # google_toggle    = local.use_google_dns ? toset(["on"]) : toset([])
 
   external_dns_values = templatefile("${path.module}/templates/external_dns_values.tmpl.yaml", {
     domain       = var.domain
