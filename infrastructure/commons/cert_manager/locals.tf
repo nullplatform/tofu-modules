53ec48f2a4c990d2ca8b4159c11113a5057f708f
Gonzalo Rojas <gonzalo.rojas@nullplatform.io>
2025-11-10 18:55:17 -0300

    fix: use ternary operator instead of coalesce for null

--- infrastructure/commons/cert_manager/locals.tf
+++ infrastructure/commons/cert_manager/locals.tf
@@ -6,17 +6,17 @@ locals {
 
     # GCP
     gcp_enabled             = var.gcp_enabled
-    gcp_service_account_key = coalesce(var.gcp_service_account_key, "")
+    gcp_service_account_key = var.gcp_service_account_key != null ? var.gcp_service_account_key : ""
 
     # Azure
     azure_enabled             = var.azure_enabled
-    azure_subscription_id     = coalesce(var.azure_subscription_id, "")
-    azure_resource_group_name = coalesce(var.azure_resource_group_name, "")
-    azure_client_id           = coalesce(var.azure_client_id, "")
-    azure_secret_key          = coalesce(var.azure_secret_key, "client-secret")
-    azure_client_secret       = coalesce(var.azure_client_secret, "")
-    azure_tenant_id           = coalesce(var.azure_tenant_id, "")
-    azure_hosted_zone_name    = coalesce(var.azure_hosted_zone_name, "")
+    azure_subscription_id     = var.azure_subscription_id != null ? var.azure_subscription_id : ""
+    azure_resource_group_name = var.azure_resource_group_name != null ? var.azure_resource_group_name : ""
+    azure_client_id           = var.azure_client_id != null ? var.azure_client_id : ""
+    azure_secret_key          = var.azure_secret_key != null ? var.azure_secret_key : "client-secret"
+    azure_client_secret       = var.azure_client_secret != null ? var.azure_client_secret : ""
+    azure_tenant_id           = var.azure_tenant_id != null ? var.azure_tenant_id : ""
+    azure_hosted_zone_name    = var.azure_hosted_zone_name != null ? var.azure_hosted_zone_name : ""
 
     # Cloudflare
     cloudflare_enabled     = var.cloudflare_enabled
