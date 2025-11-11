543ad4522567897f32acbb3ea16ca351a4b0b1ce
Mariel Vergara <vergaramariel.g@gmail.com>
2025-11-11 17:59:50 -0300

    chore: proofread and editorial review to output files

--- infrastructure/aws/vpc/output.tf
+++ infrastructure/aws/vpc/output.tf
@@ -1,14 +1,14 @@
 output "vpc_id" {
   value       = module.vpc.vpc_id
-  description = "VPC ID"
+  description = "The ID of the VPC"
 }
 
 output "private_subnets" {
   value       = module.vpc.private_subnets
-  description = "Private subnets"
+  description = "The private subnets"
 }
 
 output "public_subnets" {
   value       = module.vpc.public_subnets
-  description = "Public Subnets"
+  description = "The public subnets"
 }
--- infrastructure/azure/dns/output.tf
+++ infrastructure/azure/dns/output.tf
@@ -1,24 +1,24 @@
 output "dns_zone_name" {
-  description = "The name of the created DNS Zone"
+  description = "The name of the created DNS zone"
   value       = azurerm_dns_zone.public_dns_zone.name
 }
 
 output "dns_zone_id" {
-  description = "The ID of the DNS Zone"
+  description = "The ID of the DNS zone"
   value       = azurerm_dns_zone.public_dns_zone.id
 }
 
 output "private_dns_zone_name" {
-  description = "The name of the private created DNS Zone"
+  description = "The name of the created private DNS zone"
   value       = azurerm_dns_zone.public_dns_zone.name
 }
 
 output "private_dns_zone_id" {
-  description = "The ID of the private DNS Zone"
+  description = "The ID of the created private DNS zone"
   value       = azurerm_dns_zone.public_dns_zone.id
 }
 
 output "name_servers" {
-  description = "A list of name servers"
+  description = "The list of name servers for the DNS zone"
   value       = azurerm_dns_zone.public_dns_zone.name_servers
 }
