33fd94119aeffa19f1685459067e093b03d9292a
sebas_correa <sebastian.correa@nullplatform.io>
2025-11-13 11:22:34 -0300

    feat(agent) fix values template

--- nullplatform/agent/auth.tf
+++ nullplatform/agent/auth.tf
@@ -38,6 +38,6 @@ resource "nullplatform_api_key" "nullplatform_agent_api_key" {
   tags {
     key   = "managed-by"
     value = "IaC"
-    owner = var.nrn
+    owner = local.nrn_without_namespace
   }
 }
