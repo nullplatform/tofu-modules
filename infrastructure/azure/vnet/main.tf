5c2ab1b1f179b574df415191e7039df06f0527f0
sebas_correa <sebastian.correa@nullplatform.io>
2025-11-13 17:41:06 -0300

    feat(imple-azu) downgrade version vnet

--- infrastructure/azure/vnet/main.tf
+++ infrastructure/azure/vnet/main.tf
@@ -1,12 +1,12 @@
 
 module "avm_res_network_virtualnetwork" {
   source              = "azure/avm-res-network-virtualnetwork/azurerm"
-  version             = "v0.15.0"
+  version             = "v0.10.0"
   address_space       = var.address_space
   name                = var.vnet_name
   location            = var.location
   subnets             = var.subnets_definition
-  parent_id           = "/subscriptions/${var.subscription_id}/resourceGroups/${var.resource_group_name}"
+  resource_group_name = var.resource_group_name
 }
 
 
