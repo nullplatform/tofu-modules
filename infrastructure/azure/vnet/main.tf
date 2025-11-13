481ab4970af27397d05afb0c39f1875079d611f2
sebas_correa <sebastian.correa@nullplatform.io>
2025-11-13 08:26:21 -0300

    feat(azure-vnet): upgrade version

--- infrastructure/azure/vnet/main.tf
+++ infrastructure/azure/vnet/main.tf
@@ -1,7 +1,7 @@
 
 module "avm_res_network_virtualnetwork" {
   source              = "azure/avm-res-network-virtualnetwork/azurerm"
-  version             = "v0.10.0"
+  version             = "v0.15.0"
   address_space       = var.address_space
   name                = var.vnet_name
   location            = var.location
