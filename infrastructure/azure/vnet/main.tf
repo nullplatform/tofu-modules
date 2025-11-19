a3b8219f0376232961acba78760cfd623f40855c
Gonzalo Rojas <gonzalo.rojas@nullplatform.io>
2025-11-19 11:39:10 -0300

    chore: bump AzureRM provider to `~> 4.52` and AVM virtual network module to `v0.16.0`.

--- infrastructure/azure/vnet/main.tf
+++ infrastructure/azure/vnet/main.tf
@@ -1,10 +1,10 @@
 
 module "avm_res_network_virtualnetwork" {
-  source              = "azure/avm-res-network-virtualnetwork/azurerm"
-  version             = "v0.15.0"
-  address_space       = var.address_space
-  name                = var.vnet_name
-  location            = var.location
-  parent_id            = "/subscriptions/${var.subscription_id}/resourceGroups/${var.resource_group_name}"
-  subnets             = var.subnets_definition
+  source        = "azure/avm-res-network-virtualnetwork/azurerm"
+  version       = "v0.16.0"
+  address_space = var.address_space
+  name          = var.vnet_name
+  location      = var.location
+  parent_id     = "/subscriptions/${var.subscription_id}/resourceGroups/${var.resource_group_name}"
+  subnets       = var.subnets_definition
 }
