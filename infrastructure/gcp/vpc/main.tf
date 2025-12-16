4b6c73d4791e97893b827c66dfef8328e95b6442
Gonzalo Rojas <gonzalo.rojas@nullplatform.io>
2025-12-16 19:07:56 -0300

    Fix VPC module version constraint

--- infrastructure/gcp/vpc/main.tf
+++ infrastructure/gcp/vpc/main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source  = "terraform-google-modules/network/google"
-  version = "~> 10.0"
+  version = "~> 9.0"
 
   project_id   = var.project_id
   network_name = var.network_name
