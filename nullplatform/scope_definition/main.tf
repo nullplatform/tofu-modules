2565d6863e5477f385d04ea6970b9c22915cb748
Agustin Celentano <agustincelentano@Mac-Agustin-NullPlatform.local>
2025-12-18 15:56:34 -0300

    fix(scope): Fixing typo in annotation in scope definition module

--- nullplatform/scope_definition/main.tf
+++ nullplatform/scope_definition/main.tf
@@ -58,7 +58,7 @@ resource "nullplatform_action_specification" "from_templates" {
   results                  = jsonencode(jsondecode(base64decode(data.external.action_specs[each.key].result.json_b64)).results)
   retryable                = try(jsondecode(base64decode(data.external.action_specs[each.key].result.json_b64)).retryable, false)
   icon                     = try(jsondecode(base64decode(data.external.action_specs[each.key].result.json_b64)).icon, "")
-  annotations              = try(jsondecode(base64decode(data.external.action_specs[each.key].result.json_b64)).annotations, {})
+  annotations              = jsonencode(try(jsondecode(base64decode(data.external.action_specs[each.key].result.json_b64)).annotations, {}))
 
   lifecycle {
     ignore_changes = [name, annotations, parameters, results, type, retryable]
