fe50c75187b5b60c2e7f4286adc651a3186f3e88
sebas_correa <sebastian.correa@nullplatform.io>
2025-12-19 15:57:09 -0300

    fix(external-dns): fix sources

--- infrastructure/aws/eks/main.tf
+++ infrastructure/aws/eks/main.tf
@@ -30,7 +30,19 @@ module "eks" {
   subnet_ids               = var.aws_subnets_private_ids
   control_plane_subnet_ids = var.aws_subnets_private_ids
 
-  eks_managed_node_groups = local.eks_managed_node_groups
+  # EKS Managed Node Group(s)
+  eks_managed_node_groups = var.use_auto_mode ? {} : {
+    nullplatform = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = var.ami_type
+      instance_types = [var.instance_types]
+
+      min_size     = 2
+      max_size     = 10
+      desired_size = 2
+    }
+  }
+
 
   # ==========================================
   #  AUTO MODE
--- infrastructure/commons/external_dns/locals.tf
+++ infrastructure/commons/external_dns/locals.tf
@@ -1,6 +1,6 @@
 locals {
   base_config = {
-    sources       = ["crd", "services", "ingress"]
+    sources       = ["crd", "service", "ingress"]
     domainFilters = [var.domain_filters]
     policy        = var.policy
     txtOwnerId    = var.txt_owner_id
