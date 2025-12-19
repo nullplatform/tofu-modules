0937b93cc9c6e523c21c189d1c888ff80db384d0
sebas_correa <sebastian.correa@nullplatform.io>
2025-12-18 23:41:00 -0300

    fix(eks): segretate logic of node groups

---
+++ infrastructure/aws/eks/locals.tf
@@ -0,0 +1,14 @@
+locals {
+  # EKS Managed Node Group(s)
+  eks_managed_node_groups = var.use_auto_mode ? {} : {
+    nullplatform = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = var.ami_type
+      instance_types = [var.instance_types]
+
+      min_size     = 2
+      max_size     = 10
+      desired_size = 2
+    }
+  }
+}
--- infrastructure/aws/eks/main.tf
+++ infrastructure/aws/eks/main.tf
@@ -30,19 +30,7 @@ module "eks" {
   subnet_ids               = var.aws_subnets_private_ids
   control_plane_subnet_ids = var.aws_subnets_private_ids
 
-  # EKS Managed Node Group(s)
-  eks_managed_node_groups = var.use_auto_mode ? {} : {
-    nullplatform = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      ami_type       = var.ami_type
-      instance_types = [var.instance_types]
-
-      min_size     = 2
-      max_size     = 10
-      desired_size = 2
-    }
-  }
-
+  eks_managed_node_groups = local.eks_managed_node_groups
 
   # ==========================================
   #  AUTO MODE
