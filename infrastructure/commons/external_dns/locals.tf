ba3b6fc3ea16925e92ebd14059b549e39d0672a4
sebas_correa <sebastian.correa@nullplatform.io>
2025-12-19 23:34:06 -0300

    fix(external-dns): add source httproute

--- infrastructure/commons/external_dns/locals.tf
+++ infrastructure/commons/external_dns/locals.tf
@@ -1,6 +1,6 @@
 locals {
   base_config = {
-    sources       = ["crd", "service", "ingress"]
+    sources       = ["crd", "service", "ingress", "gateway-httproute", "gateway-grpcroute"]
     domainFilters = [var.domain_filters]
     policy        = var.policy
     txtOwnerId    = var.txt_owner_id
