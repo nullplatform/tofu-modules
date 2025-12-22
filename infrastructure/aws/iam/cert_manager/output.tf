f9e27bd092427636ef4e09e6ab3460bda61d2320
sebas_correa <sebastian.correa@nullplatform.io>
2025-12-22 18:22:53 -0300

    fix(iam-cert-manager): arn role

--- infrastructure/aws/iam/cert_manager/output.tf
+++ infrastructure/aws/iam/cert_manager/output.tf
@@ -2,3 +2,8 @@ output "nullplatform_cert_manager_role_arn" {
   description = "ARN of the cert-manager role"
   value       = module.nullplatform_cert_manager_role.arn
 }
+
+output "nullplatform_cert_manager_service_account_name" {
+  description = "Name of the SA created"
+  value = kubernetes_service_account_v1.cert_manager_acme_dns01_route53.metadata[0].name
+}
