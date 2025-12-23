1c24cc2543affd3ba814e378a53e98625bfcfb22
David Fernandez <david.fernandez@MacBook-Pro-de-First.local>
2025-12-23 17:47:10 -0300

    feat(externaldns): fix

--- infrastructure/commons/external_dns/main.tf
+++ infrastructure/commons/external_dns/main.tf
@@ -1,5 +1,5 @@
 resource "helm_release" "external_dns" {
-  name         = "external-dns"
+  name         = var.release_name
   repository   = "https://kubernetes-sigs.github.io/external-dns/"
   chart        = "external-dns"
   namespace    = var.external_dns_namespace
