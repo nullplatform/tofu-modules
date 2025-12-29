1dc1601a08178f562c3f51a18d4cc54ac7bf4a47
sebas_correa <sebastian.correa@nullplatform.io>
2025-12-29 13:02:57 -0300

    fix(route53): disable output acm

--- infrastructure/aws/route53/output.tf
+++ infrastructure/aws/route53/output.tf
@@ -18,10 +18,10 @@ output "private_zone_name" {
   value       = aws_route53_zone.private_zone.name
 }
 
-output "acm_certificate_arn" {
+/*output "acm_certificate_arn" {
   description = "The ARN of the ACM certificate associated with the Route 53 hosted zone"
   value       = module.aws_route53_acm.acm_certificate_arn
-}
+}*/
 
 output "nameservers" {
   description = "NS records for the public hosted zone"
