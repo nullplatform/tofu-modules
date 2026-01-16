4fc890f92d15a9e7fdbf00b9bc3cbb4a9b447f3a
sebas_correa <sebastian.correa@nullplatform.io>
2026-01-16 16:35:00 -0300

    fix(external-dns-iam): add trust policy

--- infrastructure/aws/iam/external_dns/main.tf
+++ infrastructure/aws/iam/external_dns/main.tf
@@ -7,7 +7,10 @@ module "nullplatform_external_dns_role" {
   oidc_providers = {
     main = {
       provider_arn               = var.aws_iam_openid_connect_provider_arn
-      namespace_service_accounts = ["external-dns:external-dns"]
+      namespace_service_accounts = [
+        "external-dns:external-dns-private",
+        "external-dns:external-dns-public",
+      ]
     }
   }
 
