01852d9b10e0a72be589566ea43720531827b21a
sebas_correa <sebastian.correa@nullplatform.io>
2026-01-16 15:58:29 -0300

    fix(external-dns): fix name chart

--- infrastructure/commons/external_dns/main.tf
+++ infrastructure/commons/external_dns/main.tf
@@ -1,5 +1,5 @@
 resource "helm_release" "external_dns" {
-  name       = "external-dns"
+  name       = "external-dns-${var.domain_filters}"
   repository = "https://kubernetes-sigs.github.io/external-dns/"
   chart      = "external-dns"
   namespace  = var.external_dns_namespace
