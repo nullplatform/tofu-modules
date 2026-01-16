c882b6e0d5433230319cef033215db238d8ad3c7
David Fernandez <david.fernandez@MacBook-Pro-de-First.local>
2026-01-16 11:11:17 -0300

    feat(): add ns

--- infrastructure/oci/vcn/outputs.tf
+++ infrastructure/oci/vcn/outputs.tf
@@ -11,10 +11,6 @@ output "vcn_all_attributes" {
   value       = module.vcn.vcn_all_attributes
 }
 
-output "view_id" {
-  description = "The OCID of the DNS private view"
-  value       = module.vcn.vcn_all_attributes.default_view_id
-}
 
 output "default_security_list_id" {
   description = "The OCID of the default security list"
