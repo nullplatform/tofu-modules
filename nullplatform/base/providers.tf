a57c91b719685e9daf3b5eb4064b181f3ae5a325
sebas_correa <sebastian.correa@nullplatform.io>
2026-01-27 16:40:08 -0300

    fix(chat-base): remove providers & fix versions providers

--- nullplatform/base/providers.tf
+++ nullplatform/base/providers.tf
@@ -9,21 +9,6 @@ terraform {
       source  = "hashicorp/helm"
       version = "~> 3.0"
     }
-
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0"
-    }
-
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=4.41.0"
-    }
-
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 5.0"
-    }
   }
 }
 
