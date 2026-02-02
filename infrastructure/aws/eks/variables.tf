bc6c307fca4a5e8ff88b94fa2baaf9ed1efa58a4
sebas_correa <sebastian.correa@nullplatform.io>
2026-02-02 09:30:56 -0300

    feat(aws-eks): add private access to k8s API

--- infrastructure/aws/eks/variables.tf
+++ infrastructure/aws/eks/variables.tf
@@ -93,3 +93,15 @@ variable "node_group_desired_size" {
   type        = number
   default     = 2
 }
+
+variable "endpoint_public_access" {
+  description = "Whether the Amazon EKS public API server endpoint is enabled"
+  type        = bool
+  default     = true
+}
+
+variable "endpoint_private_access" {
+  description = "Whether the Amazon EKS private API server endpoint is enabled"
+  type        = bool
+  default     = false
+}
