2b178e11321408c165465c8b345d1acb36a26f63
sebas_correa <sebastian.correa@nullplatform.io>
2026-02-02 15:46:32 -0300

    fix(aws-eks): fix name varible

--- infrastructure/aws/eks/main.tf
+++ infrastructure/aws/eks/main.tf
@@ -24,6 +24,7 @@ module "eks" {
 
   endpoint_public_access  = var.endpoint_public_access
   endpoint_private_access = var.endpoint_private_access
+  endpoint_public_access_cidrs = var.endpoint_public_access_cidrs
 
   # Optional: Adds the current caller identity as an administrator via cluster access entry
   enable_cluster_creator_admin_permissions = true
