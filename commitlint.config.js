57ef623285f2515428877880f4d014ca125ea443
sebas_correa <sebastian.correa@nullplatform.io>
2026-02-03 13:57:50 -0300

    fix(tofu): resolve conflicts

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,6 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'feature', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert']],
+  },
+};
