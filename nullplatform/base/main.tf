fff5fe93a9bda61b93f3450ac892f8eb57dc0784
Agustin Celentano <agustincelentano@Mac-Agustin-NullPlatform.local>
2026-02-04 14:21:24 -0300

    fix: pre-create namespaces to avoid race condition with helm chart v2.36.0+

--- nullplatform/base/main.tf
+++ nullplatform/base/main.tf
@@ -1,4 +1,31 @@
 ############################################
+# Namespaces
+# Pre-create namespaces to avoid race condition
+# with Helm chart's lookup function (chart v2.36.0+)
+############################################
+
+resource "kubernetes_namespace_v1" "nullplatform_tools" {
+  metadata {
+    name = var.namespace
+    labels = {
+      name = var.namespace
+    }
+    annotations = {
+      "openshift.io/cluster-monitoring" = "true"
+    }
+  }
+}
+
+resource "kubernetes_namespace_v1" "nullplatform_applications" {
+  metadata {
+    name = "nullplatform"
+    labels = {
+      name = "nullplatform"
+    }
+  }
+}
+
+############################################
 # Helm Release
 ############################################
 
@@ -9,7 +36,7 @@ resource "helm_release" "base" {
   namespace  = var.namespace
   version    = var.nullplatform_base_helm_version
 
-  create_namespace  = true
+  create_namespace  = false
   disable_webhooks  = false
   force_update      = true
   wait              = true
@@ -24,4 +51,9 @@ resource "helm_release" "base" {
   dependency_update = true
   max_history       = 10
   values            = [local.nullplatform_base_values]
+
+  depends_on = [
+    kubernetes_namespace_v1.nullplatform_tools,
+    kubernetes_namespace_v1.nullplatform_applications
+  ]
 }
