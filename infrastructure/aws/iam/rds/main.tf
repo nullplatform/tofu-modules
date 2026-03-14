326261625910022f4ddfe56cf5c24476ef92d4ae
sebas_correa <sebastian.correa@nullplatform.io>
2026-03-14 12:29:59 -0300

    feat(iam/rds): implement three IAM policy resources

---
+++ infrastructure/aws/iam/rds/main.tf
@@ -0,0 +1,100 @@
+################################################################################
+# RDS IAM policy
+################################################################################
+
+resource "aws_iam_policy" "rds_policy" {
+  name        = "nullplatform_${var.name}_rds_policy"
+  description = "Policy for managing RDS instances and subnet groups"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "rds:CreateDBInstance",
+          "rds:DeleteDBInstance",
+          "rds:ModifyDBInstance",
+          "rds:DescribeDBInstances",
+          "rds:CreateDBSubnetGroup",
+          "rds:DeleteDBSubnetGroup",
+          "rds:DescribeDBSubnetGroups",
+          "rds:ModifyDBSubnetGroup",
+          "rds:AddTagsToResource",
+          "rds:ListTagsForResource",
+          "rds:RemoveTagsFromResource",
+          "rds:DescribeDBParameterGroups",
+          "rds:DescribeDBParameters",
+          "rds:DescribeDBEngineVersions",
+          "rds:DescribeOrderableDBInstanceOptions",
+          "rds:DescribeOptionGroups"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+################################################################################
+# EC2 Security Group IAM policy
+################################################################################
+
+resource "aws_iam_policy" "rds_sg_policy" {
+  name        = "nullplatform_${var.name}_rds_sg_policy"
+  description = "Policy for managing EC2 security groups for RDS"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:CreateSecurityGroup",
+          "ec2:DeleteSecurityGroup",
+          "ec2:DescribeSecurityGroups",
+          "ec2:AuthorizeSecurityGroupIngress",
+          "ec2:RevokeSecurityGroupIngress",
+          "ec2:AuthorizeSecurityGroupEgress",
+          "ec2:RevokeSecurityGroupEgress",
+          "ec2:DescribeVpcs",
+          "ec2:DescribeSubnets",
+          "ec2:CreateTags",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DescribeSecurityGroupRules"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+################################################################################
+# Secrets Manager IAM policy
+################################################################################
+
+resource "aws_iam_policy" "rds_secretsmanager_policy" {
+  name        = "nullplatform_${var.name}_rds_secretsmanager_policy"
+  description = "Policy for managing Secrets Manager secrets for RDS master password"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:CreateSecret",
+          "secretsmanager:DeleteSecret",
+          "secretsmanager:DescribeSecret",
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:PutSecretValue",
+          "secretsmanager:UpdateSecret",
+          "secretsmanager:TagResource",
+          "secretsmanager:UntagResource",
+          "secretsmanager:GetResourcePolicy",
+          "secretsmanager:ListSecretVersionIds"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
