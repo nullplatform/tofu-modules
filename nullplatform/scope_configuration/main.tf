81d4994e78047556a119201bfd621cafa10b599d
Agustin Celentano <12614595+agustincelentano@users.noreply.github.com>
2026-03-30 12:33:54 -0300

    feature: add scope_configuration module for provider config resources

---
+++ nullplatform/scope_configuration/main.tf
@@ -0,0 +1,10 @@
+resource "nullplatform_provider_config" "scope_configuration" {
+  nrn        = var.nrn
+  type       = var.provider_specification_id
+  dimensions = var.dimensions
+  attributes = jsonencode(var.attributes)
+
+  lifecycle {
+    ignore_changes = [attributes]
+  }
+}
---
+++ nullplatform/scope_configuration/outputs.tf
@@ -0,0 +1,4 @@
+output "provider_config_id" {
+  description = "ID of the created provider config."
+  value       = nullplatform_provider_config.scope_configuration.id
+}
